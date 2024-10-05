b84ab5db748fff93f2fe694726a11334d3ce86f5
Hugo Belém <belem.hugo@hotmail.com>
2024-10-05 19:30:38 -0300

    / js logic

--- static/js/settingsMenu.js
+++ static/js/settingsMenu.js
@@ -1,7 +1,31 @@
-const settingsUserButton = document.querySelector('#settingsUserButton');
-const userInfo = document.querySelector('#userInfo');
+// Select all buttons and sections
+const userButton = document.querySelector('#settingsUserButton');
+const empresaButton = document.querySelector('#settingsEmpresaButton');
+const equipeButton = document.querySelector('#settingsEquipeButton');
 
-settingsUserButton.addEventListener('click', () => {
-  userInfo.classList.remove('hidden');
+const userSection = document.querySelector('#userSettings');
+const empresaSection = document.querySelector('#empresaSettings');
+const equipeSection = document.querySelector('#equipeSettings');
+
+// Function to hide all sections
+function hideAllSections() {
+  userSection.classList.add('hidden');
+  empresaSection.classList.add('hidden');
+  equipeSection.classList.add('hidden');
+}
+
+// Event listeners for each button
+userButton.addEventListener('click', () => {
+  hideAllSections(); // Hide other sections
+  userSection.classList.remove('hidden'); // Show the user settings section
 });
 
+empresaButton.addEventListener('click', () => {
+  hideAllSections();
+  empresaSection.classList.remove('hidden');
+});
+
+equipeButton.addEventListener('click', () => {
+  hideAllSections();
+  equipeSection.classList.remove('hidden');
+});
