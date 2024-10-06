c0fe995f208d1ef707a864eaa83fb823e4a16d0e
Hugo Belém <belem.hugo@hotmail.com>
2024-10-06 02:44:12 -0300

    / updated buttons

--- static/js/settingsMenu.js
+++ static/js/settingsMenu.js
@@ -16,9 +16,17 @@ function hideAllSections() {
 
 // Function to reset button styles
 function resetButtonStyles() {
-  userButton.classList.remove('bg-zinc-50');
-  empresaButton.classList.remove('bg-zinc-50');
-  equipeButton.classList.remove('bg-zinc-50');
+  userButton.classList.remove('bg-zinc-50', 'dark:bg-primary');
+  empresaButton.classList.remove('bg-zinc-50', 'dark:bg-primary');
+  equipeButton.classList.remove('bg-zinc-50', 'dark:bg-primary');
+}
+
+// Function to set the first button as selected
+function selectDefaultButton() {
+  resetButtonStyles(); // Reset styles
+  hideAllSections(); // Hide all sections
+  userSection.classList.remove('hidden'); // Show the user settings section
+  userButton.classList.add('bg-zinc-50', 'dark:bg-primary'); // Apply active styles including dark mode
 }
 
 // Event listeners for each button
@@ -26,19 +34,22 @@ userButton.addEventListener('click', () => {
   hideAllSections(); // Hide other sections
   resetButtonStyles();
   userSection.classList.remove('hidden'); // Show the user settings section
-  userButton.classList.add('bg-zinc-50'); // Apply style to clicked button
+  userButton.classList.add('bg-zinc-50', 'dark:bg-primary'); // Apply light and dark mode styles
 });
 
 empresaButton.addEventListener('click', () => {
   hideAllSections();
   resetButtonStyles();
   empresaSection.classList.remove('hidden'); // Show the empresa settings section
-  empresaButton.classList.add('bg-zinc-50'); // Apply style to clicked button
+  empresaButton.classList.add('bg-zinc-50', 'dark:bg-primary'); // Apply light and dark mode styles
 });
 
 equipeButton.addEventListener('click', () => {
   hideAllSections();
   resetButtonStyles();
   equipeSection.classList.remove('hidden'); // Show the equipe settings section
-  equipeButton.classList.add('bg-zinc-50'); // Apply style to clicked button
+  equipeButton.classList.add('bg-zinc-50', 'dark:bg-primary'); // Apply light and dark mode styles
 });
+
+// Call this function to select the first button on page load
+selectDefaultButton();
