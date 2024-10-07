39816c2be07e8356b66844e5788ea05e1eee1046
Hugo Belém <belem.hugo@hotmail.com>
2024-10-07 14:58:26 -0300

    / settingsButtons style

--- static/js/settingsMenu.js
+++ static/js/settingsMenu.js
@@ -16,9 +16,18 @@ function hideAllSections() {
 
 // Function to reset button styles
 function resetButtonStyles() {
-  userButton.classList.remove('bg-zinc-50', 'dark:bg-primary');
-  empresaButton.classList.remove('bg-zinc-50', 'dark:bg-primary');
-  equipeButton.classList.remove('bg-zinc-50', 'dark:bg-primary');
+  userButton.classList.add(
+    'ring-zinc-400', 'text-zinc-400',
+    'dark:ring-zinc-500', 'dark:text-zinc-500'
+  );
+  empresaButton.classList.add(
+    'ring-zinc-400', 'text-zinc-400',
+    'dark:ring-zinc-500', 'dark:text-zinc-500'
+  );
+  equipeButton.classList.add(
+    'ring-zinc-400', 'text-zinc-400',
+    'dark:ring-zinc-500', 'dark:text-zinc-500'
+  );
 }
 
 // Function to set the first button as selected
@@ -26,7 +35,14 @@ function selectDefaultButton() {
   resetButtonStyles(); // Reset styles
   hideAllSections(); // Hide all sections
   userSection.classList.remove('hidden'); // Show the user settings section
-  userButton.classList.add('bg-zinc-50', 'dark:bg-primary'); // Apply active styles including dark mode
+  userButton.classList.remove(
+    'ring-zinc-400', 'text-zinc-400',
+    'dark:ring-zinc-500', 'dark:text-zinc-500'
+  );
+  userButton.classList.add(
+    'ring-black', 'text-black',
+    'dark:ring-zinc-300', 'dark:text-zinc-300'
+  );
 }
 
 // Event listeners for each button
@@ -34,21 +50,42 @@ userButton.addEventListener('click', () => {
   hideAllSections(); // Hide other sections
   resetButtonStyles();
   userSection.classList.remove('hidden'); // Show the user settings section
-  userButton.classList.add('bg-zinc-50', 'dark:bg-primary'); // Apply light and dark mode styles
+  userButton.classList.remove(
+    'ring-zinc-400', 'text-zinc-400',
+    'dark:ring-zinc-500', 'dark:text-zinc-500'
+  );
+  userButton.classList.add(
+    'ring-black', 'text-black',
+    'dark:ring-zinc-300', 'dark:text-zinc-300'
+  );
 });
 
 empresaButton.addEventListener('click', () => {
   hideAllSections();
   resetButtonStyles();
   empresaSection.classList.remove('hidden'); // Show the empresa settings section
-  empresaButton.classList.add('bg-zinc-50', 'dark:bg-primary'); // Apply light and dark mode styles
+  empresaButton.classList.remove(
+    'ring-zinc-400', 'text-zinc-400',
+    'dark:ring-zinc-500', 'dark:text-zinc-500'
+  );
+  empresaButton.classList.add(
+    'ring-black', 'text-black',
+    'dark:ring-zinc-300', 'dark:text-zinc-300'
+  );
 });
 
 equipeButton.addEventListener('click', () => {
   hideAllSections();
   resetButtonStyles();
   equipeSection.classList.remove('hidden'); // Show the equipe settings section
-  equipeButton.classList.add('bg-zinc-50', 'dark:bg-primary'); // Apply light and dark mode styles
+  equipeButton.classList.remove(
+    'ring-zinc-400', 'text-zinc-400',
+    'dark:ring-zinc-500', 'dark:text-zinc-500'
+  );
+  equipeButton.classList.add(
+    'ring-black', 'text-black',
+    'dark:ring-zinc-300', 'dark:text-zinc-300'
+  );
 });
 
 // Call this function to select the first button on page load
