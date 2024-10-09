9e28be1181a2efcac24f66aa625792f1a3f27edd
Hugo Belém <belem.hugo@hotmail.com>
2024-10-09 19:55:36 -0300

    + Settings class

--- erp/views.py
+++ erp/views.py
@@ -3,18 +3,23 @@ from django.shortcuts import render, redirect
 
 from users.forms import CustomUserChangeForm
 
+class Settings:
+    def user(request) -> dict:
+        user = request.user
+        form = CustomUserChangeForm(instance=user)
+
+        if request.method == "POST":
+            form = CustomUserChangeForm(request.POST, instance=user)
+            if form.is_valid():
+                form.save()
+                return redirect('erp:index')
+
+        context = {'form': form}
+        return context
+
 
 @login_required(login_url='login')
 def erp(request):
-    user = request.user
-    form = CustomUserChangeForm(instance=user)
-
-    if request.method == "POST":
-        form = CustomUserChangeForm(request.POST, instance=user)
-        if form.is_valid():
-            form.save()
-            return redirect('erp:index')
-
-    context = {'form': form}
+    context = Settings.user(request)
 
     return render(request, 'erp/index.html', context) 
