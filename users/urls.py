4a00603b2fb91b8553ef4fd4736a1f2f0d2b04a2
Hugo Belém <belem.hugo@hotmail.com>
2024-10-09 12:21:31 -0300

    + logout path

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from django.conf import settings
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
 app_name = 'users'
 urlpatterns = [
     path('login/', views.signin, name='signIn'),
-    path('sair/', views.logout, name='signOut')
+    path(
+        'sair/',
+        LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL),
+        name='logout'
+    ),    
 ]
