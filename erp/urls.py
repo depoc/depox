973940f88dd303ca240170d4801ff6ce185405c2
Hugo Belém <belem.hugo@hotmail.com>
2024-10-10 11:22:48 -0300

    + password_change

--- erp/urls.py
+++ erp/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 app_name = 'erp'
 urlpatterns = [
-    path('', views.erp, name='index')
+    path('', views.erp, name='index'),
+
+    path(
+        'alterar-senha/',
+        auth_views.PasswordChangeView.as_view(),
+        name='password_change',
+    ),
 ]
