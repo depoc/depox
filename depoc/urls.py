8c16541ecc83ed64c856a723df7af54b54a8647d
Hugo Belém <belem.hugo@hotmail.com>
2024-10-10 18:34:33 -0300

    + path to login

--- depoc/urls.py
+++ depoc/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('erp/', include('erp.urls')),
+
+    # temporary: while the index page is not built
+    path(
+        '',
+        TemplateView.as_view(template_name='users/login.html'),
+        name='login'
+    ),
 ]
