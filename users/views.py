a8cc16d8f55e65c610a08a48c686652433f99e07
Hugo Belém <belem.hugo@hotmail.com>
2024-10-11 00:21:30 -0300

    - manual msg

--- users/views.py
+++ users/views.py
@@ -49,8 +49,6 @@ def register(request):
 
             login(request, user)
             return redirect('erp:index')
-        else:
-            messages.error(request, 'dados inválidos :(')
 
     context = {'form': form}
     return render(request, 'users/register.html', context)
