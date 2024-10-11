7864c214af112b3707eb281ea26a9509be680728
Hugo Belém <belem.hugo@hotmail.com>
2024-10-11 11:54:09 -0300

    + CompanyForm

---
+++ erp/forms.py
@@ -0,0 +1,23 @@
+from django.forms import ModelForm
+
+from .models import Company
+
+
+class CompanyForm(ModelForm):
+    model = Company
+    fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(CompanyForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': '''
+                    w-full
+                    text-xs
+                    text-end
+                    bg-transparent
+                    text-black/40
+                    dark:text-white/40
+                    outline-none''',
+            })    
