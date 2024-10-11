db1e5aba0cb44ce4c1ffbb909b6f15528696eed0
Hugo Belém <belem.hugo@hotmail.com>
2024-10-11 12:03:18 -0300

    + format_cnpj

--- erp/templatetags/filters.py
+++ erp/templatetags/filters.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def first_name(value):
     return value.split()[0].lower() if value else ''
+
+@register.filter
+def format_cnpj(cnpj):
+    return f'{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:14]}'
