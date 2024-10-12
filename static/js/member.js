7afe4e85b35176bcc0fc8fd4c817b3a94c5141f8
Hugo Belém <belem.hugo@hotmail.com>
2024-10-12 12:56:18 -0300

    + hide equipe list

--- static/js/member.js
+++ static/js/member.js
@@ -2,8 +2,12 @@ document.getElementById('toggle-membro-button').addEventListener('click', functi
     // Toggle visibility of the member and CNPJ forms
     const memberForm = document.getElementById('membro-form');
     memberForm.classList.toggle('hidden');
+
+    const equipeList = document.getElementById('equipe-list');
+    equipeList.classList.toggle('hidden');
   
     const areaMemberButton = document.getElementById('div-membro-button');
+    areaMemberButton.classList.toggle('mt-8');
     areaMemberButton.classList.toggle('border-black/30');
     areaMemberButton.classList.toggle('dark:border-white/60');
 
