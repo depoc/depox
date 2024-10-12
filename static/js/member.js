200a920e9ae6ce13783fab75ac223718799f710e
Hugo Belém <belem.hugo@hotmail.com>
2024-10-12 12:27:13 -0300

    + js: member form

---
+++ static/js/member.js
@@ -0,0 +1,10 @@
+document.getElementById('toggle-membro-button').addEventListener('click', function() {
+    // Toggle visibility of the member and CNPJ forms
+    const memberForm = document.getElementById('membro-form');
+    memberForm.classList.toggle('hidden');
+  
+    const areaMemberButton = document.getElementById('div-membro-button');
+    areaMemberButton.classList.toggle('border-black/30');
+    areaMemberButton.classList.toggle('dark:border-white/60');
+
+});
