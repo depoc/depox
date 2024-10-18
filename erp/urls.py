e0e406ef69b291263573b9250d4981dc63fd59f4
Hugo Belém <belem.hugo@hotmail.com>
2024-10-18 18:37:46 -0300

    changed url paths

--- erp/urls.py
+++ erp/urls.py
@@ -6,17 +6,13 @@ app_name = 'erp'
 urlpatterns = [
     path('', views.erp, name='index'),
 
-    path('excluir/', views.Settings.account_delete, name='delete'),
+    path('conta/excluir/', views.account_delete, name='account_delete'),
 
-    path(
-        'membro/<int:pk>/excluir',
-         views.Settings.member_delete, 
-         name='member_delete'
-    ),
+    path('membro/<int:pk>/excluir', views.member_delete, name='member_delete'),
 
     path(
         'alterar-senha/',
-        views.Settings.PasswordChange.as_view(),
+        views.PasswordChange.as_view(),
         name='password_change',
     ),
 ]
