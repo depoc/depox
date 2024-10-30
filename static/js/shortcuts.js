b11ab9f5d57d447df39f6371e8fb59048cae0227
Hugo Belém <belem.hugo@hotmail.com>
2024-10-29 23:29:59 -0300

    added keyboard shortcuts

---
+++ static/js/shortcuts.js
@@ -0,0 +1,11 @@
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'f') {
+        window.location.href = '/caixa';
+    }
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'h') {
+        window.location.href = '/erp';
+    }
+});
