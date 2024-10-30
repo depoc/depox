ec62a5fbf784555d41520aa3aba483d92e6507d1
Hugo Belém <belem.hugo@hotmail.com>
2024-10-29 23:52:03 -0300

    added shortcut to floating menu

--- static/js/erp/mobileMenu.js
+++ static/js/erp/mobileMenu.js
@@ -3,16 +3,33 @@ const modal = document.getElementById('mobileMenu');
 const closeModal = document.getElementById('closeMobileMenu');
 
 modalButton.addEventListener('click', () => {
-modal.classList.remove('hidden');
-modal.classList.add('flex');
+  modal.classList.remove('hidden');
+  modal.classList.add('flex');
 });
 
 closeModal.addEventListener('click', () => {
-modal.classList.add('hidden');
+  modal.classList.add('hidden');
+  modal.classList.remove('flex'); // Ensures 'flex' class is removed
 });
 
 modal.addEventListener('click', (event) => {
-if (event.target === modal) {
-  modal.classList.add('hidden');
-}
+  if (event.target === modal) {
+    modal.classList.add('hidden');
+    modal.classList.remove('flex'); // Ensures 'flex' class is removed
+  }
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key.toLowerCase() === 'm') {
+    if (modal.classList.contains('hidden')) {
+      modal.classList.remove('hidden');
+      modal.classList.add('flex');
+    }
+  }
+  if (event.key === 'Escape') { 
+    if (modal.classList.contains('flex')) {
+      modal.classList.remove('flex');
+      modal.classList.add('hidden');
+    }
+  }
 });
