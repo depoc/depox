f55418869f44e8273a7f682f2c55a07288d1d256
Hugo Belém <belem.hugo@hotmail.com>
2024-11-02 18:28:33 -0300

    connecting accounts to backend

--- finance/views.py
+++ finance/views.py
@@ -2,8 +2,17 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from erp.services import Settings
+from .models import BankAccount
 
 @login_required(login_url='users:login')
 def caixa(request):
-    context = Settings.context(request)
+    company = request.user.company
+    banks = BankAccount.objects.filter(company=company)
+    saldos = []
+    for bank in banks:
+        saldos.append(bank.saldo)
+    saldo_total = sum(saldos)
+    
+    context:dict = Settings.context(request)
+    context.update({'saldo_total': saldo_total})
     return render(request, 'finance/caixa.html', context)
