901e88686f69951e70a1efeb9e7cece5d865cf9d
Hugo Belém <belem.hugo@hotmail.com>
2024-11-02 16:45:31 -0300

    added create_bank_account signal

---
+++ finance/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from erp.models import Company
+from .models import BankAccount
+
+
+@receiver(post_save, sender=Company)
+def create_back_account(sender, instance, created, **kwargs):
+    if created:
+        BankAccount.objects.create(
+            bank = 'caixa',
+            company=instance,
+        )
