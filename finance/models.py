0341bf19b996e39f7ccbf4fcde1a6686e3cef50a
Hugo Belém <belem.hugo@hotmail.com>
2024-11-02 16:44:37 -0300

    created BackAccount model

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from erp.models import Company
+
+
+class BankAccount(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name='banks',
+    )
+    name = models.CharField(max_length=255)
+    agencia = models.CharField(max_length=255, blank=True, null=True)
+    conta = models.CharField(max_length=255, blank=True, null=True)
+    saldo = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True, default=0,
+    )
+
+    def __str__(self):
+        return f'{self.bank}: {self.company.cnpj}'
