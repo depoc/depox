a36d69d378c390b71d28ce1ab3c9b161b72064a3
Hugo Belém <belem.hugo@hotmail.com>
2024-11-17 15:42:08 -0300

    added transaction form

--- finance/forms.py
+++ finance/forms.py
@@ -7,3 +7,19 @@ class TransactionsForm(ModelForm):
     class Meta:
         model = Transactions
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(TransactionsForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({
+                'autofocus': True,
+                'class': '''
+                    w-fit
+                    placeholder:text-black/40 dark:placeholder:text-white/50
+                    bg-transparent
+                    outline-none''',
+            })            
+
+        if 'valor' in self.fields:
+            self.fields['valor'].widget.attrs.update({'placeholder': '00,00'})            
