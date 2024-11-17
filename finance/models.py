d92fd19adc0222a1bca9bb4fd78ee4d326737b0c
Hugo Belém <belem.hugo@hotmail.com>
2024-11-17 13:50:00 -0300

    removed default value from transaction.valor

--- finance/models.py
+++ finance/models.py
@@ -23,7 +23,7 @@ class Transactions(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     tipo = models.CharField(max_length=255, blank=False, null=False)
     valor = models.DecimalField(
-        max_digits=10, decimal_places=2, blank=True, null=True, default=0,
+        max_digits=10, decimal_places=2, blank=True, null=True,
     )
     conta = models.ForeignKey(BankAccount, on_delete=models.CASCADE)
     contato = models.CharField(max_length=255, blank=True, null=True)
