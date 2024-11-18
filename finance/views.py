508adda8d0ce9c9af2aa9f903976b2b3e506ce0b
Hugo Belém <belem.hugo@hotmail.com>
2024-11-18 00:59:09 -0300

    added decorator to access caixa

--- finance/views.py
+++ finance/views.py
@@ -1,11 +1,14 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
 
 from erp.services import Settings
 from .services import Finance
 
 
-@login_required(login_url='users:login')
+def has_company(user):
+    return hasattr(user, 'company') and user.company is not None
+
+@user_passes_test(has_company, login_url='users:login')
 def caixa(request):
     context = Settings.context(request)
     context.update(Finance.context(request))
