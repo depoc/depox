9182103dfe358f27d99f803bf78e35f998623ce2
Hugo Belém <belem.hugo@hotmail.com>
2024-11-17 22:43:41 -0300

    added signals to sum transactions

--- finance/signals.py
+++ finance/signals.py
@@ -1,8 +1,10 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.db import transaction
+from django.db.models import Sum
 
 from erp.models import Company
-from .models import BankAccount
+from .models import BankAccount, Transactions
 
 
 @receiver(post_save, sender=Company)
@@ -12,3 +14,21 @@ def create_bank_account(sender, instance, created, **kwargs):
             name = 'caixa',
             company=instance,
         )
+
+@receiver(post_save, sender=Transactions)
+def update_bank_account_balance(sender, instance, **kwargs):
+    with transaction.atomic():
+        bank_account = instance.conta
+        total_debit = Transactions.objects.filter(conta=bank_account).aggregate(Sum('valor'))['valor__sum'] or 0
+        bank_account.saldo = total_debit
+        bank_account.save()        
+
+@receiver(post_delete, sender=Transactions)
+def update_bank_account_balance_on_delete(sender, instance, **kwargs):
+    # Inicia uma transação para garantir que tudo seja atômico
+    with transaction.atomic():
+        bank_account = instance.conta
+        # Recalculate the total balance for the bank account after the transaction is deleted
+        total_debit = Transactions.objects.filter(conta=bank_account).aggregate(Sum('valor'))['valor__sum'] or 0
+        bank_account.saldo = total_debit
+        bank_account.save()        
