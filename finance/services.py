6773097dcb571d9455e3071514324986374425de
Hugo Belém <belem.hugo@hotmail.com>
2024-11-17 21:46:05 -0300

    bug fix adicionar saldo

--- finance/services.py
+++ finance/services.py
@@ -25,13 +25,22 @@ class Finance:
 
     @staticmethod
     def add_transactions(request) -> dict:
-        form = TransactionsForm()
+        form = TransactionsForm()     
+        conta = request.POST.get('conta')
+        if conta:
+            bank = BankAccount.objects.get(id=conta)
+        tipo = request.POST.get('tipo')         
 
         if request.method == 'POST' and 'add-transaction' in request.POST:
             post_data = request.POST.copy()
             valor = post_data.get('valor', '')
             valor_cleaned = valor.replace('.', '').replace(',', '.')
             post_data['valor'] = valor_cleaned
+
+            if tipo == 'entrada': 
+                saldo = float(bank.saldo) + float(valor_cleaned)
+                bank.saldo = saldo
+                bank.save()
             
             form = TransactionsForm(post_data)
             
