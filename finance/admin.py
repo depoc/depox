aecc611b89acf3f2490b3be664f3dce629608b46
Hugo Belém <belem.hugo@hotmail.com>
2024-11-17 23:52:49 -0300

    added list_display to Transaction admin panel

--- finance/admin.py
+++ finance/admin.py
@@ -6,5 +6,9 @@ from .models import BankAccount, Transactions
 class BankAccountAdmin(admin.ModelAdmin):
     list_display = ('name', 'company', 'saldo')
 
+
+class TransactionsAdmin(admin.ModelAdmin):
+    list_display = ('tipo', 'valor', 'conta', 'created')
+
 admin.site.register(BankAccount, BankAccountAdmin)
-admin.site.register(Transactions)
+admin.site.register(Transactions, TransactionsAdmin)
