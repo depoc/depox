e8b6bc8d218ee1269a835f42406a62c7f86c7c99
Hugo Belém <belem.hugo@hotmail.com>
2024-11-19 12:08:24 -0300

    added transaction detail logic

--- static/js/finance/detail.js
+++ static/js/finance/detail.js
@@ -1,9 +1,27 @@
 const history = document.getElementById('history');
 const closeHistory = document.getElementById('closeHistory');
 
-function historyButton() {
+function historyButton(row) {
   history.classList.remove('hidden');
   history.classList.add('flex')
+
+  // Access data attributes
+  const conta = row.getAttribute('data-conta');
+  const tipo = row.getAttribute('data-tipo');
+  const valor = row.getAttribute('data-valor');
+  const contato = row.getAttribute('data-contato');
+  const descricao = row.getAttribute('data-descricao');
+  const categoria = row.getAttribute('data-categoria');
+  const created = row.getAttribute('data-created');
+
+  // Populate modal fields
+  document.getElementById('modal-conta').textContent = conta;
+  document.getElementById('modal-tipo').textContent = tipo;
+  document.getElementById('modal-valor').textContent = valor;
+  document.getElementById('modal-contato').textContent = contato;
+  document.getElementById('modal-descricao').textContent = descricao;
+  document.getElementById('modal-categoria').textContent = categoria;  
+  document.getElementById('modal-created').textContent = created;  
   }
 
 closeHistory.addEventListener('click', () => {
