ccef412ff6331c2a4bcb6ee266b397c431f99f17
Hugo Belém <belem.hugo@hotmail.com>
2024-11-29 17:33:16 -0300

    updated Contacts fields & added to admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contacts
+
+
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf_cnpj', 'company', 'tipo')
+
+admin.site.register(Contacts, ContactsAdmin)
--- contacts/models.py
+++ contacts/models.py
@@ -12,22 +12,25 @@ class Contacts(models.Model):
     pessoa = models.CharField(max_length=255)
     cpf_cnpj = models.CharField(max_length=18, unique=True)
     contribuinte = models.CharField(max_length=255, blank=True)
-    ie = models.IntegerField()
-    im = models.IntegerField()
-    tipo = models.CharField(max_length=255)
+    ie = models.IntegerField(blank=True, null=True)
+    im = models.IntegerField(blank=True, null=True)
+    tipo = models.CharField(max_length=255, blank=True)
     status = models.CharField(max_length=255, blank=True)
 
     celular = models.CharField(max_length=14, blank=True)
     email = models.EmailField(blank=True)
 
     endereco = models.CharField(max_length=255, blank=True)
-    numero = models.PositiveIntegerField(blank=True)
+    numero = models.PositiveIntegerField(blank=True, null=True)
     bairro = models.CharField(max_length=255, blank=True)
     complemento = models.CharField(max_length=255, blank=True)
     cidade = models.CharField(max_length=255, blank=True)
     uf = models.CharField(max_length=2, blank=True)    
     cep = models.CharField(max_length=9, blank=True)
 
-    nascimento = models.DateField()
+    nascimento = models.DateField(blank=True, null=True)
     observacoes = models.CharField(max_length=255, blank=True)
-    anexos = models.FileField(upload_to='media/anexos')
+    anexos = models.FileField(upload_to='media/anexos', blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
