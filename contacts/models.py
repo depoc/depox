90ac707c31f410e70b3590ab2d204132cfae66b1
Hugo Belém <belem.hugo@hotmail.com>
2024-11-29 17:17:47 -0300

    created model Contacts

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from erp.models import Company
+
+
+class Contacts(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    nome = models.CharField(max_length=255)
+    apelido = models.CharField(max_length=255, blank=True)    
+    pessoa = models.CharField(max_length=255)
+    cpf_cnpj = models.CharField(max_length=18, unique=True)
+    contribuinte = models.CharField(max_length=255, blank=True)
+    ie = models.IntegerField()
+    im = models.IntegerField()
+    tipo = models.CharField(max_length=255)
+    status = models.CharField(max_length=255, blank=True)
+
+    celular = models.CharField(max_length=14, blank=True)
+    email = models.EmailField(blank=True)
+
+    endereco = models.CharField(max_length=255, blank=True)
+    numero = models.PositiveIntegerField(blank=True)
+    bairro = models.CharField(max_length=255, blank=True)
+    complemento = models.CharField(max_length=255, blank=True)
+    cidade = models.CharField(max_length=255, blank=True)
+    uf = models.CharField(max_length=2, blank=True)    
+    cep = models.CharField(max_length=9, blank=True)
+
+    nascimento = models.DateField()
+    observacoes = models.CharField(max_length=255, blank=True)
+    anexos = models.FileField(upload_to='media/anexos')
