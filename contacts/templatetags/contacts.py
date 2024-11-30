0d8370960d35963a75fffea05b750be8cab827f0
Hugo Belém <belem.hugo@hotmail.com>
2024-11-30 15:21:37 -0300

    added format_cpf_cnpj filter

--- contacts/templatetags/contacts.py
+++ contacts/templatetags/contacts.py
@@ -4,6 +4,16 @@ register = template.Library()
 
 @register.filter
 def format_phone_number(phone: str) -> str:
-    if phone:
-        return f'{phone[:2]} {phone[2:7]}-{phone[7:11]}'
-    return 'adicionar celular'
+    return f'{phone[:2]} {phone[2:7]}-{phone[7:11]}' if phone else ''
+
+@register.filter
+def format_cpf_cnpj(cpf_cnpj: str) -> str:
+    if cpf_cnpj and len(cpf_cnpj) == 11:
+        cpf = f'{cpf_cnpj[:3]}.{cpf_cnpj[3:6]}.{cpf_cnpj[6:9]}-{cpf_cnpj[9:11]}'
+        return cpf
+    
+    cnpj = (
+        f'{cpf_cnpj[0:2]}.{cpf_cnpj[2:5]}.{cpf_cnpj[5:8]}'
+        f'/{cpf_cnpj[8:12]}-{cpf_cnpj[12:14]}'
+    )
+    return cnpj
