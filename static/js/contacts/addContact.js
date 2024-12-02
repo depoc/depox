335616000e1f96f87a61d3eadd463a0815c46c1b
Hugo Belém <belem.hugo@hotmail.com>
2024-12-02 10:20:21 -0300

    added add_contact.html modal

---
+++ static/js/contacts/addContact.js
@@ -0,0 +1,28 @@
+const addContactButton = document.getElementById("addContactButton");
+const addContact = document.getElementById("addContact");
+const closeAddContact = document.getElementById("closeAddContact");
+
+addContactButton.addEventListener("click", () => {
+    addContact.classList.remove("hidden");
+    addContact.classList.add("flex");
+})
+
+closeAddContact.addEventListener("click", () => {
+    addContact.classList.remove("flex");
+    addContact.classList.add("hidden");
+})
+
+addContact.addEventListener("click", (e) => {
+    if (e.target === addContact) {
+        addContact.classList.add("hidden");
+    }
+})
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') { 
+      if (addContact.classList.contains('flex')) {
+        addContact.classList.remove('flex');
+        addContact.classList.add('hidden');
+      }
+    }
+});
