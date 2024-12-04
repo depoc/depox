72e25c9a6893e2ec8b242f517e7a2179e1e0d6b8
Hugo Belém <belem.hugo@hotmail.com>
2024-12-03 21:55:17 -0300

    set digits limit & changed format of formatPhone.js

--- static/js/erp/formatPhone.js
+++ static/js/erp/formatPhone.js
@@ -11,7 +11,8 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     function formatPhone(phone) {
-        phone = phone.replace(/\D/g, '');
-        return phone.replace(/(\d{2})(\d{5})(\d{4})/, '($1)$2-$3');
+        phone = phone.replace(/\D/g, ''); // Remove non-digit characters
+        phone = phone.substring(0, 11);  // Limit to a maximum of 11 digits
+        return phone.replace(/(\d{2})(\d{5})(\d{4})/, '$1 $2-$3');
     }
 });
