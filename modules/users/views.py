95e3056d062301ebb7b12000cbece0d6d02a4671
Hugo Belém <belem.hugo@hotmail.com>
2026-01-11 22:21:06 +0000

    fix: redirect authenticated users to erp:index

--- modules/users/views.py
+++ modules/users/views.py
@@ -1,31 +1,21 @@
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from django.shortcuts import render, redirect
 
 from .models import User
-from .forms import (
-    CustomUserCreationForm,
-    CustomUserChangeForm,
-)
+from .forms import CustomUserCreationForm
 
 
 def signin(request):
-
     if request.user.is_authenticated:
         return redirect('erp:index')
 
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        try:
-            user = User.objects.get(username=username)
-        except:
-            messages.error(request, '')
 
         user = authenticate(request, username=username, password=password)
-
-        if user is not None:
+        if user:
             login(request, user)
             return redirect('erp:index')
         else:
