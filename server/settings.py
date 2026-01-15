69dc91de8f84720d96d801c025e4fa7ae3df1f11
Hugo Belém <belem.hugo@hotmail.com>
2026-01-15 22:47:40 +0000

    fix: add contacts modal

--- server/settings.py
+++ server/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 if DEBUG:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        'default': dj_database_url.config(
+            default=os.getenv('DATABASE_URL'),
+            conn_max_age=600,
+        )
     }
 else:
     DATABASES = {
